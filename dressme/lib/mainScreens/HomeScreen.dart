5b5addfe3fd754a1eb5cb153d38b49469ddf7622
Beyza Nur <111365090+BeyzaNurEkiz@users.noreply.github.com>
2023-12-08 23:33:57 +0300

    splash

---
+++ dressme/lib/mainScreens/HomeScreen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello Beyza'),
+      ),
+      body: const Center(
+        child: Text(
+          'Hello Beyza',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ dressme/lib/splash screen/splash.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+
+import 'package:dressme/main.dart';
+import 'package:dressme/mainScreens/HomeScreen.dart';
+import 'package:flutter/material.dart';
+
+class MySplashScreen extends StatefulWidget {
+  const MySplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 3), () async {
+      Navigator.push(context, MaterialPageRoute(builder: (c) => HomeScreen()));
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 207, 70, 241),
+              Color.fromARGB(255, 72, 70, 228),
+            ],
+            begin: FractionalOffset(0.0, 0.0),
+            end: FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+            tileMode: TileMode.clamp,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/welcome.png"),
+              const SizedBox(
+                height: 10,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
