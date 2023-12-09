8b57b8ec47ae3e95212272c81747adfaacccf791
Harun Sefa İnan <99213895+harunsefainan@users.noreply.github.com>
2023-12-10 01:55:47 +0300

    firebase

--- dressme/lib/main.dart
+++ dressme/lib/main.dart
@@ -1,7 +1,13 @@
-import 'package:dressme/routes/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'routes/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MainApp());
 }
 
