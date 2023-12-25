a0274a332fd1511d8f8ed062fb384c3f73caa148
Beyza Nur <111365090+BeyzaNurEkiz@users.noreply.github.com>
2023-12-25 19:01:28 +0300

    Update profile_screen.dart

--- dressme/lib/routes/profile_screen.dart
+++ dressme/lib/routes/profile_screen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dressme/global/global.dart';
 import 'package:dressme/routes/authentication/auth_screen.dart';
-import 'package:dressme/routes/authentication/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
